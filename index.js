cf361ffcabc2ca9e9b0c6ad507068b033db945b2
Branden Mosley <branden.mosley1337@gmail.com>
2018-01-21 18:47:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var katzDeliLine = []
 
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.shift(name)
-  return `Welcome, ${name}. You are ${katzDeliLine.length} in line.`
+  return `Welcome, ${name}. You are ${katzDeliLine.length + 1} in line.`
 }
