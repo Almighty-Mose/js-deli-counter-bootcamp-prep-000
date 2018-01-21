fbb568a21882a8b578db6f4bd7c91bda9774887e
Branden Mosley <branden.mosley1337@gmail.com>
2018-01-21 20:11:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,9 +14,8 @@ function nowServing(katzDeliLine) {
 }
 
 function currentLine(katzDeliLine) {
-  for (var i = 0, )
-  if (katzDeliLine.length > 0) {
-    return 
+    if (katzDeliLine.length > 0) {
+    return `The line is currently:`
   } else {
     return "The line is currently empty."
   }
