2c596d98a607f5c7ffc51768b85a681b9069d82f
Branden Mosley <branden.mosley1337@gmail.com>
2018-01-21 19:01:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,5 +9,7 @@ function nowServing() {
   if (katzDeliLine.length > 0) {
   return `Currently serving ${katzDeliLine[0]}.`
   katzDeliLine.shift()
-  
+  }else{
+    return "There is nobody waiting to be served!"
+  }
 }
