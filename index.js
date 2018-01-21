d6d9b5fbe0ca5c6bae6819436e2dfb4469284db2
Branden Mosley <branden.mosley1337@gmail.com>
2018-01-21 19:22:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ function takeANumber(katzDeliLine, name) {
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
 
-function nowServing() {
+function nowServing(katzDeliLine) {
   return `Currently serving ${katzDeliLine[0]}.`
 }
